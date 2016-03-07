87b0d8bc6cc5a6baecfd48ae0a6eb4eaa2045fae
xGioe <giovanni@mriva.me>
2016-03-07 13:47:29 +0100

    Add generate_word() method

--- Hangman/CoderDojoBot.py
+++ Hangman/CoderDojoBot.py
@@ -1,5 +1,7 @@
 import sys, time, telepot
 import string
+from os.path import getsize
+from random import randint
 
 class CoderDojoBot(telepot.Bot):
     """Bot class"""
@@ -9,14 +11,14 @@ class CoderDojoBot(telepot.Bot):
         self.TOKEN = '187053440:AAF199All4GbW5moBpq8tga_SEvQbFNvG88'
         super(CoderDojoBot, self).__init__(self.TOKEN)
         self.show_keyboard = self.setKeyboard()
-        self.choosen_word= 'null'
+        self.choosen_word = self.generateWord()
 
     ### Handle
 
     def on_chat_message(self,msg):
         message_type, visibility, user_id = telepot.glance(msg)
         self.user_id = user_id
-        message = 'Ciao'
+        message = 'ciao'
         self.printKeyboard(message)
 
     ### Game
@@ -26,6 +28,16 @@ class CoderDojoBot(telepot.Bot):
     # Print correct/wrong answer
 
     # Choose random word from file
+    def generateWord(self):
+        dic_path ="DictionaryE.txt"
+        file_size =getsize (dic_path )
+        file_in =open (dic_path ,"rb")
+        while True :
+            offset =randint (0 ,file_size )
+            file_in .seek (offset )
+            L =file_in .read (25 ).split ("\r\n")
+            if len (L )>2 :
+                return L [1 ]
 
     ### Keyboard
 
