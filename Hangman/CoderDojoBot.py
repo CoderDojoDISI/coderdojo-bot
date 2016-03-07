4fe455e0dd01774577ab027284c91d7cca614306
xGioe <giovanni@mriva.me>
2016-03-07 13:30:16 +0100

    Add print keyboard method; Move setKeyboard to init_method

--- Hangman/CoderDojoBot.py
+++ Hangman/CoderDojoBot.py
@@ -8,12 +8,32 @@ class CoderDojoBot(telepot.Bot):
     def __init__(self):
         self.TOKEN = '187053440:AAF199All4GbW5moBpq8tga_SEvQbFNvG88'
         super(CoderDojoBot, self).__init__(self.TOKEN)
+        self.show_keyboard = self.setKeyboard()
+        self.choosen_word= 'null'
+
+    ### Handle
 
     def on_chat_message(self,msg):
         message_type, visibility, user_id = telepot.glance(msg)
-        show_keyboard = self.setKeyboard()
-        self.sendMessage(user_id, 'This is a custom keyboard', reply_markup=show_keyboard)
+        self.user_id = user_id
+        message = 'Ciao'
+        self.printKeyboard(message)
 
+    ### Game
+
+    # Check if guess belongs to choosen_word
+
+    # Print correct/wrong answer
+
+    # Choose random word from file
+
+    ### Keyboard
+
+    # Print keyboard with given "message"
+    def printKeyboard(self,message):
+        self.sendMessage(self.user_id, message, reply_markup=self.show_keyboard)
+
+    # Generate keyboard
     def setKeyboard(self):
         alphabetList = list(string.ascii_uppercase)
         first_row = alphabetList[0:6]
